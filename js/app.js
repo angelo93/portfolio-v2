4fa48a41611df77a2af718f3e232fdc103148592
Michael Navarro <navarro_michael93@gmail.com>
2019-07-01 14:23:09 -0500

    Fix Navbar resetting on refresh

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 //---- Navbar resizing ----//
-$(function() {
-  $(document).scroll(function() {
-    var $nav = $("#mainNavbar");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-  });
-});
+// $(function() {
+//   $(document).scroll(function() {
+//     var $nav = $("#mainNavbar");
+//     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//   });
+// });
+
+var $nav = $("#mainNavbar");
 
 //---- Navbar collapse on click ----//
 $(document).ready(function() {
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse("hide");
   });
+});
+
+// ---- Navbar scroll animation logic ----//
+$(window).scroll(scrollChange);
+function scrollChange() {
+  if ($(document).scrollTop() > $nav.height()) {
+    $("#mainNavbar").addClass("scrolled");
+  } else {
+    $("#mainNavbar").removeClass("scrolled");
+  }
+}
+
+$(document).ready(function() {
+  scrollChange();
 });
